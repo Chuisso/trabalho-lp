4732a812560b6a327bea4a93bafbd45f641d5519
Wesley <Wesley@Wesley-PC>
2017-11-10 22:51:28 -0200

    Atualizado a base dos personagens e começo do jogador

--- src/trabalholp/BasePersonagens.java
+++ src/trabalholp/BasePersonagens.java
@@ -1,19 +1,27 @@
 package trabalholp;
 
+import java.util.Random;
+
 public class BasePersonagens {
     protected int vida;
     protected int vidaMax;
     
-    private int dano;
     
-    public BasePersonagens(int vida, int dano) {
+    public BasePersonagens(int vida) {
         this.vida = vida;
-        this.vidaMax = vida;
-        this.dano = dano;
+        this.vidaMax = vida;   
     }
     
-    public void Atacar(BasePersonagens inimigo) {
-        inimigo.addVida(vida);
+    protected int atacar(BasePersonagens inimigo, double danoMin, double danoMax) {
+        Random rand = new Random();
+        
+        float dano;
+        do {
+            dano = Math.round(rand.nextInt((int)Math.round(danoMax))+1);
+        } while (dano < danoMin);
+        
+        inimigo.addVida(-Math.round(dano));
+        return Math.round(dano);
     }
     
     public int getVida() {
@@ -25,24 +33,16 @@ public class BasePersonagens {
     }
     
     public void addVida(int qntd) {
-        int vidaNova = vida-qntd;
+        int vidaNova = vida+qntd;
         if(vidaNova > vidaMax) {
             vida = vidaMax;
         }
-        else if(vidaNova <= 0) {
+        else if(vidaNova < 0) {
             vida = 0;
         }
         else {
             vida += qntd;
         }
-    }
-
-    public int getDano() {
-        return dano;
-    }
-
-    public void setDano(int dano) {
-        this.dano = dano;
     }
   
 }
